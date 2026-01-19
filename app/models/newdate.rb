c890345096502f111472cb2c0f339c24965a8304
Anzu <alexandros_0201@icloud.com>
2026-01-20 01:04:52 +0900

    rails newコマンドを実行し、アプリケーションを作成

---
+++ app/models/newdate.rb
@@ -0,0 +1,2 @@
+class Newdate < ApplicationRecord
+end
---
+++ db/migrate/20260119151207_create_newdates.rb
@@ -0,0 +1,12 @@
+class CreateNewdates < ActiveRecord::Migration[7.2]
+  def change
+    create_table :newdates do |t|
+      t.string :title
+      t.integer :startday
+      t.integer :endday
+      t.string :memo
+
+      t.timestamps
+    end
+  end
+end
